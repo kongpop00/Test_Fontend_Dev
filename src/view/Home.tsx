030eb5eacb30fd932fd0e9e9f718af0cba2b8a39
K-ongpop Nualsaard <kongpopku@ku.th>
2024-02-24 08:30:46 +0700

    Update View Home

--- src/view/Home.tsx
+++ src/view/Home.tsx
@@ -1,11 +1,48 @@
-
+import { Card, Flex } from "antd";
+import { Link } from "react-router-dom";
 
 const Home = () => {
+  interface datatype {
+    title: String;
+    sunTitle: String;
+    link: String;
+  } 
+  const datatypes:datatype[] = [
+    {
+      title: "Test 1",
+      sunTitle: "asdasd",
+      link: "/test1",
+    },
+    {
+      title: "Test 2",
+      sunTitle: "asdasd",
+      link: "sadas",
+    },
+    {
+      title: "Test 3",
+      sunTitle: "asdasd",
+      link: "sadas",
+    },
+  ];
   return (
-    <div>
-      Home
-    </div>
-  )
-}
+    <Flex
+    className="viewblackground"
+     style={{width:"100%" , height:"100vh ", backgroundColor:"red"}}
+     justify="center"
+     align="center"
+     gap={30}
+     >
+        { datatypes.map(( datatype,index)=>{
+          return(
 
-export default Home
+           <Link to={`${datatype.link}`}> <Card key={index} title={datatype.title}  style={{ width: 300 }}>
+         <p>{datatype.sunTitle}</p>
+      
+    </Card></Link>
+          )
+        })}
+    </Flex>
+  );
+};
+
+export default Home;
